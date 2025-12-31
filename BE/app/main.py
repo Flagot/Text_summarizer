7cc93ff3bb526db9939b27d1ddd62eaad2228103
Flagot Mulugeta <flagotmulugeta3@gmail.com>
2025-12-31 17:13:34 +0300

    fix cors

--- BE/app/main.py
+++ BE/app/main.py
@@ -24,9 +24,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",  # Vite default port (can vary)
-        "http://localhost:3000",
+      "https://text-summarizer-azure.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
